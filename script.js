548e3c3873b0f8688646c9878b0cfa0a09d288d6
janvihegde <160409058+janvihegde@users.noreply.github.com>
2024-10-30 21:52:09 +0530

    Add files via upload
    sign up

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("password").addEventListener("click", function() {
+    const passwordField = document.getElementById("password");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+});
